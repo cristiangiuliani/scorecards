54a574f3009da759c6451e16119586701fb1341e
cristiangiuliani <info@cristiangiuliani.com>
2025-09-30 12:44:37 +0200

    updated fearGreed headers

--- api/fetchFearGreed.ts
+++ api/fetchFearGreed.ts
@@ -6,13 +6,14 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0'); // mesi 0-based
     const day = String(today.getDate()).padStart(2, '0');
-
     const dateStr = `${year}-${month}-${day}`;
 
     const url = `https://production.dataviz.cnn.io/index/fearandgreed/graphdata/${dateStr}`;
 
     const response = await fetch(url, {
-      headers: { 'User-Agent': 'Mozilla/5.0' },
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+      },
     });
 
     if (!response.ok) throw new Error(`Status ${response.status}`);
